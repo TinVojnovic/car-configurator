4f1b69956ce4912f2aa1b0262569369f46530740
Tin Vojnovic <tin.vojnovi1@gmail.com>
2022-07-26 15:03:44 +0200

    init develop commit

---
+++ src/shared/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import {css} from "@emotion/react"
+
+const header = {
+    
+    backgroundColor: 'red'
+}
+
+export const Navbar: React.FC = () => {
+    return (
+        <div style={header}>
+            MJAU
+        </div>
+    )
+}
