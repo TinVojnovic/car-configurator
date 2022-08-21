86d70f0b6d6f5f24092e2db5687d7a029153d272
Tin Vojnovic <tin.vojnovi1@gmail.com>
2022-08-22 01:46:03 +0200

    ConfigView was missing the ConfigurationDetails component for some reason. Noticed it when self-reviewing the code.

--- src/views/config-view/ConfigView.tsx
+++ src/views/config-view/ConfigView.tsx
@@ -1,10 +1,12 @@
 import { Outlet} from 'react-router-dom';
 import { ImagePreview } from '../../modules';
+import { ConfigurationDetails } from '../../modules/config-view/configurator-details';
 
 export const ConfigView: React.FC = () => {
     return (
         <div>
             <ImagePreview type='exterior' maxIndex={5}/>
+            <ConfigurationDetails/>
             <Outlet />
         </div>
     )
